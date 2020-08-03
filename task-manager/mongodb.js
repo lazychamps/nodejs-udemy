e49329b203b13b034d8c01936153bd74a583e420
Tapan Kumar <tapankumar@qainfotech.com>
2020-08-03 12:59:55 +0530

    Example for insertOne with error handling

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,9 +15,17 @@ MongoClient.connect(
     }
 
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "Harvey",
-      age: 43,
-    });
+    db.collection("users").insertOne(
+      {
+        name: "Harvey",
+        age: 43,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );

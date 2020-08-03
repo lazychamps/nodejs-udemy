c18fb9656c01babba3ace5d9df12f6aa2ea5ac84
Tapan Kumar <tapankumar@qainfotech.com>
2020-08-03 17:43:38 +0530

    validation and sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { default: validator } = require("validator");
 
 const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api";
 
@@ -7,34 +8,51 @@ mongoose.connect(connectionUrl, {
   useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
-//   name: {
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+});
+
+//Valid
+const user = new User({
+  name: "  Louis    ",
+  email: "LOUIS@litt.com      ",
+});
+//Invalid
+// const user = new User({ name: "Harvey", age: "age" });
+
+user.save().then(console.log).catch(console.log);
+
+// const Task = mongoose.model("Task", {
+//   description: {
 //     type: String,
 //   },
-//   age: {
-//     type: Number,
+//   completed: {
+//     type: Boolean,
 //   },
 // });
 
-// //Valid
-// const user = new User({ name: "Harvey", age: 43 });
-// //Invalid
-// // const user = new User({ name: "Harvey", age: "age" });
+// const myTask = new Task({
+//   description: "Learn ReactJS",
+//   completed: true,
+// });
 
-// user.save().then(console.log).catch(console.log);
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-  },
-});
-
-const myTask = new Task({
-  description: "Learn ReactJS",
-  completed: true,
-});
-
-myTask.save().then(console.log).catch(console.log);
+// myTask.save().then(console.log).catch(console.log);

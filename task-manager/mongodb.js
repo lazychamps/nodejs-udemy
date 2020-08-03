fb0827da67b16360b38d9002a5ad3acae900a008
Tapan Kumar <tapankumar@qainfotech.com>
2020-08-03 12:51:38 +0530

    Connecting and Inserting Documents

---
+++ task-manager/mongodb.js
@@ -0,0 +1,23 @@
+//CRUD create read update and delete
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+
+    const db = client.db(databaseName);
+    db.collection("users").insertOne({
+      name: "Harvey",
+      age: 43,
+    });
+  }
+);

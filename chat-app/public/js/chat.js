145ea6bffc2b1c51b5ccfd164624cdd9b7cf87dc
Tapan Kumar <tapankumar@qainfotech.com>
2020-08-22 15:08:22 +0530

    Forms and button states

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -2,6 +2,7 @@ const socket = io();
 
 const $messageForm = document.querySelector("#message-form");
 const $inputField = document.querySelector("[name='message']");
+const $submitButton = document.querySelector("#save-btn");
 const $locationButton = document.querySelector("#send-location");
 
 socket.on("message", (message) => {
@@ -10,8 +11,12 @@ socket.on("message", (message) => {
 
 $messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  $submitButton.setAttribute("disabled", "disabled");
   const message = $inputField.value;
   socket.emit("sendMessage", message, (error) => {
+    $submitButton.removeAttribute("disabled");
+    $inputField.value = "";
+    $inputField.focus();
     if (error) {
       return console.log(error);
     }
@@ -23,10 +28,12 @@ $locationButton.addEventListener("click", (e) => {
   if (!navigator.geolocation) {
     return alert("Your browser does not support Geolocation");
   }
+  $locationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition(({ coords }) => {
     const { latitude, longitude } = coords;
     socket.emit("sendLocation", { latitude, longitude }, () => {
+      $locationButton.removeAttribute("disabled");
       console.log("Location has been sent!!");
     });
   });
